6c3e32ececaf72c7c424effc9b46f99a0e9b38c1
Chris Maloney <voldrani@gmail.com>
2013-10-29 03:13:25 -0400

    Add properties to Java config

--- src/AppConfig.java
+++ src/AppConfig.java
@@ -5,7 +5,9 @@ import org.chrismaloney.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 // In a Java configured project, this class, with its annotations, takes the place
 // of applicationContext.xml.
@@ -16,7 +18,16 @@ import org.springframework.context.annotation.Scope;
 
 // Use this for autowiring:
 @ComponentScan({"org.chrismaloney"})
+
+// Import properties
+@PropertySource("app.properties")
+
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	// This identifies the following as the method to use to get a particular bean
 	// Note that the name of the *method* doesn't matter.  But it's a nice idea to
--- src/org/chrismaloney/repository/HibernateCustomerRepositoryImpl.java
+++ src/org/chrismaloney/repository/HibernateCustomerRepositoryImpl.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.chrismaloney.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
-	/* (non-Javadoc)
-	 * @see org.chrismaloney.repository.CustomerRepository#findAll()
-	 */
-	@Override
+	@Value("${someProperty}")
+	private String someValue;
+	
 	public List<Customer> findAll() {
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
-		customer.setGivenName("Fred");
+		customer.setGivenName(someValue);
 		customer.setFamilyName("Johnson");
 		customers.add(customer);
 		
