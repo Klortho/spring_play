becf4ad7d62b76b056e66f44e9b1775b31d28423
Chris Maloney <voldrani@gmail.com>
2013-10-29 03:08:09 -0400

    Add properties

--- src/org/chrismaloney/repository/HibernateCustomerRepositoryImpl.java
+++ src/org/chrismaloney/repository/HibernateCustomerRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.chrismaloney.model.Customer;
 import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Value;
 
 
 // This annotation "wires" this implementation to the interface.
@@ -15,15 +16,14 @@ import org.springframework.stereotype.Repository;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
-	/* (non-Javadoc)
-	 * @see org.chrismaloney.repository.CustomerRepository#findAll()
-	 */
-	@Override
+	@Value("${someProperty}")
+	private String someValue;
+
 	public List<Customer> findAll() {
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
-		customer.setGivenName("Fred");
+		customer.setGivenName(someValue);
 		customer.setFamilyName("Johnson");
 		customers.add(customer);
 		
